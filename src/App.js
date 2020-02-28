97d7c856c2cc81389e8668d80e4c5b7e9f97c5f8
Arun Mohan <arunmh7@gmail.com>
2020-02-29 00:48:05 +0530

    Added shop with routing, collection item, preview item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shoppage/shoppage.component';
 
 import './App.scss';
 
 function App() {
   return (
-    <HomePage />
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/shop" component={ShopPage} />
+    </Switch>
   );
 }
 
---
+++ src/pages/shoppage/shoppage.component.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import SHOP_DATA from '../../config-files/shop.data';
+
+import PreviewCollection from '../../components/preview-collection/preview-collection.component';
+
+class ShopPage extends Component {
+    state = {
+        collections: SHOP_DATA
+    }
+
+    render() {
+        return (
+            this.state.collections.map(
+                ({ id, ...otherCollectionProps }) => <PreviewCollection key={id} {...otherCollectionProps} />
+            )
+        )
+    }
+}
+
+export default ShopPage;
