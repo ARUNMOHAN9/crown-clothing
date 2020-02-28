7f8b5452bac793c883d4cab70c72c3991ade3788
Arun Mohan <arunmh7@gmail.com>
2020-02-28 23:56:39 +0530

    added images

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './directory.styles.scss';
 
-import sections from '../../config-files/directory.data';
+import SECTION_DATA from '../../config-files/directory.data';
 
 import MenuItem from '../menu-item/menu-item.component';
 
 const Directory = () => (
 
     <div className='directoryMenu'>
-        {sections.map(({title, id, imageUrl, size}) => <MenuItem key={id} title={title} imgUrl={imageUrl} size={size}/>)}
+        {SECTION_DATA.map(({title, id, imageUrl, size}) => <MenuItem key={id} title={title} imgUrl={imageUrl} size={size}/>)}
     </div>
 )
 
