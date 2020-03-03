70b4f686c862bdc511005e4a78c15d32526c6411
Arun Mohan <arunmh7@gmail.com>
2020-03-03 21:08:27 +0530

    added collection,collection-item

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -6,7 +6,9 @@ const CollectionItem = ({ name, price, imageUrl }) => {
     return (
        <div className="collection-item">
            <div className="image"
-           style={{backgroundImage: `url(${imageUrl})`}}></div>
+           style={{backgroundImage: `url(${imageUrl})`}}>
+               <button className="main-btn cust-btn">ADD TO CART</button>
+           </div>
            <div className="collection-footer">
                <span className="name">{name}</span>
                <span className="price">{price}</span>
