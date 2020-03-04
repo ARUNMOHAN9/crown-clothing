3e1842988d187eb1be20f090e25fa769b77c9d05
Arun Mohan <arunmh7@gmail.com>
2020-03-05 00:13:53 +0530

    input field bugfix

--- src/components/custom-input/custom-input.component.jsx
+++ src/components/custom-input/custom-input.component.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './custom-input.styles.scss'
 
 const CustomInput = ({ label, trigger, ...otherProps }) => {
+    const [valueLength, changeValue] = useState(0);
     return (
         <div className="group">
-            <input className="form-input" onChange={trigger} {...otherProps} />
+            <input className="form-input" onChange={(e) => {changeValue(e.target.value.length)}} {...otherProps} />
             {
                 label ? 
-                <label className={`${otherProps?.value?.length ? 'shrink' : ''} form-input-label`}>{label}</label> : null
+                <label className={`${valueLength ? 'shrink' : ''} form-input-label`}>{label}</label> : null
             }
         </div>
     );
