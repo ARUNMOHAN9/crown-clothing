97f5c41cd206b306ce1d96263a0f610b90f4ad3e
Arun Mohan <arunmh7@gmail.com>
2020-03-06 15:31:24 +0530

    implemented signin and added styles

--- src/components/signIn/signin.component.jsx
+++ src/components/signIn/signin.component.jsx
@@ -1,21 +1,48 @@
 import React from 'react';
 
-import {signInWithGoogle} from '../../config-files/firebase/firebase.utils';
+import { signInWithGoogle, auth } from '../../config-files/firebase/firebase.utils';
 
-import  CustomInput from '../custom-input/custom-input.component';
+import './signin.component.styles.scss';
+
+import CustomInput from '../custom-input/custom-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-const SignIn = () => {
-    return(
-        <div className="signIn_wrapper">
-            <h1>I already have an account</h1>
-            <h2>Sign in with your email and password</h2>
-            <CustomInput type={'email'} label={'Email'} required/>
-            <CustomInput type={'password'} label={'Password'} required/>
-            <CustomButton type="submit" classes="primary-btn">Submit</CustomButton>
-            <CustomButton type="button" classes="secondary-btn" onClick={signInWithGoogle}>Sign in with Google</CustomButton>
-        </div>
-    );
+class SignIn extends React.Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleSubmit = async (e) => {
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    handleChange = (e) => {
+        const key = e.target.name;
+        this.setState({ [key]: e.target.value });
+    }
+
+    render() {
+        return (
+            <div className="signIn_wrapper">
+                <h1>I already have an account</h1>
+                <h2>Sign in with your email and password</h2>
+                <CustomInput trigger={this.handleChange} value={this.state.email} name={'email'} type={'email'} label={'Email'} required />
+                <CustomInput trigger={this.handleChange} value={this.state.password} name={'password'} type={'password'} label={'Password'} required />
+                <div className="btn-container">
+                    <CustomButton trigger={this.handleSubmit} type="submit" classes="primary-btn">Sign in</CustomButton>
+                    <CustomButton type="button" classes="secondary-btn" onClick={signInWithGoogle}>Sign in with Google</CustomButton>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default SignIn;
--- src/pages/signin-signup-page/signin-signup-page.jsx
+++ src/pages/signin-signup-page/signin-signup-page.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './signin-signup-page.styles.scss';
+
 import SignIn from '../../components/signIn/signin.component';
 import SignUp from '../../components/signup/signup.component';
 
