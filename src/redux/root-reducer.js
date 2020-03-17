a5949fa367c357125cbf954a359bcc3d4b7a6f99
Arun Mohan <arunmh7@gmail.com>
2020-03-17 11:04:45 +0530

    added react persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,22 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
+
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 import loaderReducer from './loader/loader.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    whitelist: ['cart'] // only cart will be persisted
+};
+
+const rootReducer = combineReducers({
     'user': userReducer,
     'cart': cartReducer,
     'loader': loaderReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
